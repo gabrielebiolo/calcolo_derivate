33f05f8661ca21121c2a086b93a62ea65c0a7ff2
Gabriele Biolo <60276263+gabrielebiolo@users.noreply.github.com>
2021-12-07 22:32:19 +0100

    rimozione parti inutili

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# import sympy as sp
-# import scipy as sgraph
-# func = input()
-# func = func.strip()
-# if "^" in func:
-#     func = func.replace("^", "**")
-# x = sp.Symbol('x')
-# y = x**3 + 5
-# yd = sp.diff(y,x)
-# print(str(yd).replace("**", "^").replace("*", ""))
-
 from sympy import *
 from sympy.parsing.sympy_parser import (split_symbols, 
                                         standard_transformations, 
